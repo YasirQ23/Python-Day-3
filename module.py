894aa8503245d74bf70968c2dc2d6c1df29c74df
Yasir Qureshi <yasirqureshi@Yasirs-MacBook-Air.local>
2022-04-28 14:18:50 -0400

    first commit

---
+++ module.py
@@ -0,0 +1,11 @@
+from math import pi
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+
+def area(x,y):
+    """ Takes in 2 inputs (x,y) which are length and width and returns area"""
+    return x*y
+
+def circumference(radius):
+    """Takes input (radius) and returns circumference"""
+    return (radius*2)*pi
